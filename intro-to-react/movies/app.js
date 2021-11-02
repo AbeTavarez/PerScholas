fc60a2244587ebd03d468b60ade5a28b3e643008
abeTavarez <abeincwireless@gmail.com>
2021-11-01 21:48:07 -0400

    movies app cp

--- intro-to-react/movies/app.js
+++ intro-to-react/movies/app.js
@@ -4,27 +4,61 @@ console.log(`running script...`);
 class App extends React.Component {
     state = {
         baseURL: 'http://www.omdbapi.com/?',
-        apikey: 'apikey=' + '',
+        apikey: 'apikey=' + '36e3873b',
         query: '&t=',
+        searchURL: '',
+        movie: '',
         movieTitle: '',
-        searchURL: ''
     }
 
     handleChange = e => {
-        this.setState({[e.target.name] : e.target.value})
+        this.setState({[e.target.id] : e.target.value})
     }
 
     handleSubmit = e => {
         e.preventDefault()
-        console.log(this.state.movieTitle)
+        this.setState({
+            searchURL: this.state.baseURL + this.state.apikey + this.state.query + this.state.movieTitle
+        }, () => {
+            fetch(this.state.searchURL)
+            .then(res => {
+                return res.json()
+            })
+            .then(json => this.setState({
+                movie: json,
+                movieTitle: ''
+            }), err => console.log(err))
+        })
     }
     render() {
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="searchTerm">Search</label>
-                    <input type="text" onChange={this.handleChange} name="movieTitle"/>
+                    <label htmlFor="movieTitle">Movie Title</label>
+                    <input 
+                        type="text" 
+                        value={this.state.movieTitle}
+                        onChange={this.handleChange} 
+                        id="movieTitle"
+                        />
+                    <input type="submit" value="Find Movie Info"/>
                 </form>
+
+                { this.state.movie && <MovieInfo movie={this.state.movie}/>}
+            </div>
+        )
+    }
+}
+
+class MovieInfo extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Title:{this.props.movie.Title}</h1>
+                <h2>Year: {this.props.movie.Year}</h2>
+                <img src={this.props.movie.Poster}/>
+                <h3>Genre: {this.props.movie.Genre}</h3>
+                <h4>Plot: {this.props.movie.Plot}</h4>
             </div>
         )
     }

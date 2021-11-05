753e91568b2fe75ee88b47c300d8cd4e552d9743
abeTavarez <abeincwireless@gmail.com>
2021-11-05 16:31:53 -0400

    redux init

---
+++ intro-to-react/search-product/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <div>
+        <h1>App</h1>
+    </div>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
